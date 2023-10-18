b6c0fea9fb9439b6a7b4a10049797a82ede48e66
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-10-18 01:27:15 -0400

    feat: using next-connect to manage routes (backend setup)

---
+++ app/api/rooms/route.ts
@@ -0,0 +1,18 @@
+import { allRooms } from '@/backend/controllers/roomControllers';
+import { createEdgeRouter } from 'next-connect';
+import { NextRequest } from 'next/server';
+
+interface RequestContext {
+  // most of the time ID will be inside params (room ID, user ID, etc)
+  params: {
+    id: string;
+  };
+}
+
+const router = createEdgeRouter<NextRequest, RequestContext>();
+
+router.get(allRooms);
+
+export async function GET(request: NextRequest, ctx: RequestContext) {
+  return router.run(request, ctx);
+}
