ecb89aec70f5081a63706470105d58772178f156
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-10-19 15:52:18 -0400

    feat: add GET single room details routing

--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -1,21 +1,21 @@
 import dbConnect from '@/backend/config/dbConnect';
-import { allRooms, newRoom } from '@/backend/controllers/roomControllers';
+import { getRoomDetails } from '@/backend/controllers/roomControllers';
 import { createEdgeRouter } from 'next-connect';
 import { NextRequest } from 'next/server';
 
-interface RequestContext {}
+interface RequestContext {
+  // most of the time ID will be inside params (room ID, user ID, etc)
+  params: {
+    id: string;
+  };
+}
 
 const router = createEdgeRouter<NextRequest, RequestContext>();
 
 dbConnect();
 
-router.get(allRooms);
-router.post(newRoom);
+router.get(getRoomDetails);
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
-  return router.run(request, ctx);
-}
-
-export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -25,3 +25,26 @@ export const newRoom = async (req: NextRequest) => {
     room,
   });
 };
+
+export const getRoomDetails = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: 'Room not found',
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
