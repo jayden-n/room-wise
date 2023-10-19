a1207fc3fe2b49c46ad06ae2ce5a2a920099b3ff
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-10-19 15:17:09 -0400

    refactor: implement data seeding & compile to JS

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -2,14 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 import Room from '../models/room';
 
 export const allRooms = async (req: NextRequest) => {
+  // pagination displaying
+  const resPerPage: number = 8;
+
+  // GET request
+  const rooms = await Room.find();
+
   return NextResponse.json({
-    data: 'hello',
+    success: true,
+    resPerPage,
+    rooms,
   });
 };
 
 export const newRoom = async (req: NextRequest) => {
   const body = await req.json();
-
+  // POST request
   const room = await Room.create(body);
 
   return NextResponse.json({
---
+++ seeder/seeder.ts
@@ -0,0 +1,25 @@
+import Room from '../backend/models/room';
+import mongoose from 'mongoose';
+import { rooms } from './data';
+
+const seedRooms = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/room-wise');
+
+    await Room.deleteMany();
+    console.log('Rooms are deleted');
+
+    // add rooms to database
+    await Room.insertMany(rooms);
+    console.log('Rooms are added');
+
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    // exit immediately if got error
+    process.exit();
+  }
+};
+
+// must call
+seedRooms();
