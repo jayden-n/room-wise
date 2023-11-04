bf00ff296feea0f3c2dfcd4121ad10d0fe39123f
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-11-03 21:10:38 -0400

    fix (front-end): add login component adn testing

---
+++ components/layout/ButtonLoader.tsx
@@ -0,0 +1,5 @@
+const ButtonLoader = () => {
+	return <div className='lds-dual-ring'></div>;
+};
+
+export default ButtonLoader;
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,68 +1,75 @@
 'use client';
 
-import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
-  return (
-    <nav className='navbar sticky-top py-2'>
-      <div className='container'>
-        <div className='col-6 col-lg-3 p-0'>
-          <div className='navbar-brand'>
-            <a href='/'>
-              {/* <img
+	return (
+		<nav className='navbar sticky-top py-2'>
+			<div className='container'>
+				<div className='col-6 col-lg-3 p-0'>
+					<div className='navbar-brand'>
+						<a href='/'>
+							{/* <img
                 style={{ cursor: 'pointer' }}
                 src='../../public/images/logo.png'
                 alt='RoomWise'
               /> */}
-              Room Wise
-            </a>
-          </div>
-        </div>
+							Room Wise
+						</a>
+					</div>
+				</div>
 
-        <div className='col-6 col-lg-3 mt-3 mt-md-0 text-end'>
-          <div className='ml-4 dropdown d-line'>
-            <button
-              className='btn dropdown-toggle'
-              type='button'
-              id='dropdownMenuButton1'
-              data-bs-toggle='dropdown'
-              aria-expanded='false'
-            >
-              <figure className='avatar avatar-nav'>
-                <img
-                  src='/images/default_avatar.jpg'
-                  alt='John Doe'
-                  className='rounded-circle placeholder-glow'
-                  height='50'
-                  width='50'
-                />
-              </figure>
+				<div className='col-6 col-lg-3 mt-3 mt-md-0 text-end'>
+					<div className='ml-4 dropdown d-line'>
+						<button
+							className='btn dropdown-toggle'
+							type='button'
+							id='dropdownMenuButton1'
+							data-bs-toggle='dropdown'
+							aria-expanded='false'
+						>
+							<figure className='avatar avatar-nav'>
+								<img
+									src='/images/default_avatar.jpg'
+									alt='John Doe'
+									className='rounded-circle placeholder-glow'
+									height='50'
+									width='50'
+								/>
+							</figure>
 
-              <span className='placeholder-glow ps-1'>John Doe</span>
-            </button>
+							<span className='placeholder-glow ps-1'>John Doe</span>
+						</button>
 
-            <div
-              className='dropdown-menu w-100'
-              aria-labelledby='dropdownMenuButton1'
-            >
-              <a href='/admin/dashboard' className='dropdown-item'>
-                Dashboard
-              </a>
-              <a href='/bookings/me' className='dropdown-item'>
-                My Bookings
-              </a>
-              <a href='/me/update' className='dropdown-item'>
-                Profile
-              </a>
-              <a href='/' className='dropdown-item text-danger'>
-                Logout
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+						<div
+							className='dropdown-menu w-100'
+							aria-labelledby='dropdownMenuButton1'
+						>
+							<a href='/admin/dashboard' className='dropdown-item'>
+								Dashboard
+							</a>
+							<a href='/bookings/me' className='dropdown-item'>
+								My Bookings
+							</a>
+							<a href='/me/update' className='dropdown-item'>
+								Profile
+							</a>
+							<a href='/' className='dropdown-item text-danger'>
+								Logout
+							</a>
+						</div>
+					</div>
+
+					<Link
+						href='/login'
+						className='btn btn-danger px-4 text-white login-header-btn float-right'
+					>
+						Login
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
