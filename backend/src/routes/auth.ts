d226412b8ec40c501e25b5ed65e78681181efd95
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-02-08 13:45:56 -0500

    feat(server): add in user logout api endpoint that creates a new token that expires immediately

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -76,4 +76,11 @@ router.get('/validate-token', verifyToken, (req: Request, res: Response) => {
 	res.status(200).send({ userId: req.userId });
 });
 
+router.post('/logout', (req: Request, res: Response) => {
+	// set a new token that expires immediately
+	res.cookie('auth_token', '', {
+		expires: new Date(0), // immediately
+	});
+});
+
 export default router;
