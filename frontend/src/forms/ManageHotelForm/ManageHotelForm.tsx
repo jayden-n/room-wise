7f03c9865af503238995fa236c7331d2291fee70
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-02-13 16:37:50 -0500

    feat: implement hotel details section with react-hook-form

---
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -0,0 +1,28 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import DetailsSection from './DetailsSection';
+
+export type HotelFormData = {
+	name: string;
+	city: string;
+	country: string;
+	description: string;
+	type: string;
+	pricePerNight: number;
+	starRating: number;
+	facilities: string[];
+	imageFiles: FileList;
+	adultCount: number;
+	childCount: number;
+};
+
+const ManageHotelForm = () => {
+	const formMethods = useForm<HotelFormData>();
+	return (
+		<FormProvider {...formMethods}>
+			<form>
+				<DetailsSection />
+			</form>
+		</FormProvider>
+	);
+};
+export default ManageHotelForm;
