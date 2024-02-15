0b402388c0da50cfa8fe809008c734a652e86e04
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-02-15 13:56:03 -0500

    fix: bug when user cannot submit add-hotel form data

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -21,14 +21,48 @@ export type HotelFormData = {
 
 const ManageHotelForm = () => {
 	const formMethods = useForm<HotelFormData>();
+	const { handleSubmit } = formMethods;
+
+	// formDataJSON is from our front-end form
+	const onSubmit = handleSubmit((formDataJSON: HotelFormData) => {
+		// create new FormData object & call our API
+		const formData = new FormData();
+		formData.append('name', formDataJSON.name);
+		formData.append('city', formDataJSON.city);
+		formData.append('country', formDataJSON.country);
+		formData.append('description', formDataJSON.description);
+		formData.append('pricePerNight', formDataJSON.pricePerNight.toString());
+		formData.append('starRating', formDataJSON.starRating.toString());
+		formData.append('adultCount', formDataJSON.adultCount.toString());
+		formData.append('childCount', formDataJSON.childCount.toString());
+
+		// facilities: string[];
+		formDataJSON.facilities.forEach((facility, index) => {
+			formData.append(`facility[${index}]`, facility); // how you send array to server when working with formData
+		});
+
+		Array.from(formDataJSON.imageFiles).forEach((imageFile) => {
+			formData.append('imageFile', imageFile); // multer will handle this
+		});
+	});
+
 	return (
 		<FormProvider {...formMethods}>
-			<form className="flex flex-col gap-12">
+			<form onSubmit={onSubmit} className="flex flex-col gap-12">
 				<DetailsSection />
 				<TypesSection />
 				<FacilitiesSection />
 				<GuestsSection />
 				<ImagesSection />
+
+				<span className="flex justify-end">
+					<button
+						type="submit"
+						className="bg-sky-500 text-white p-2 font-bold hover:bg-sky-400 text-xl rounded-md"
+					>
+						Add hotel
+					</button>
+				</span>
 			</form>
 		</FormProvider>
 	);
