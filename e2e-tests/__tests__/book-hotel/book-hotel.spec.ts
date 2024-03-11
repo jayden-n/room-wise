025fa25fa80a47b6f084238475c09f60a326a433
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-03-11 16:00:41 -0400

    refactor: add in my bookings page e2e test

--- e2e-tests/__tests__/book-hotel/book-hotel.spec.ts
+++ e2e-tests/__tests__/book-hotel/book-hotel.spec.ts
@@ -46,4 +46,7 @@ test('should book hotel', async ({ page }) => {
 
 	await page.getByRole('button', { name: 'Confirm Booking' }).click();
 	await expect(page.getByText('Booking Saved!')).toBeVisible();
+
+	await page.getByRole('link', { name: 'My Bookings' }).click();
+	await expect(page.getByText('Khach san 5 sao')).toBeVisible();
 });
