77023310644413bb19871e9e5556b72a51ddd03d
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-03-11 16:25:03 -0400

    refactor: add in most recent destinations in homepage

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -203,6 +203,15 @@ export const searchHotels = async (
 	return response.json();
 };
 
+export const fetchHotels = async (): Promise<HotelType[]> => {
+	const response = await fetch(`${API_BASE_URL}/api/hotels`);
+	if (!response.ok) {
+		throw new Error('Error fetching hotel');
+	}
+
+	return response.json();
+};
+
 export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
 	const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`);
 
